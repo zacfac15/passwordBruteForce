04e50911514ba8f534057a8c070658e873a2c2a7
fabianzach <zacfac15@new.edvhtl.at>
2019-03-26 09:37:04 +0100

    edited Producer

--- src/main/java/htlkaindorf/zach/exa_403_bruteforce/Producer.java
+++ src/main/java/htlkaindorf/zach/exa_403_bruteforce/Producer.java
@@ -1,12 +1,44 @@
 package htlkaindorf.zach.exa_403_bruteforce;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Producer implements Runnable
 {
+
+  private Password passwd;
+  private String file = System.getProperty("user.dir")
+          + File.separator + "src"
+          + File.separator + "res"
+          + File.separator + "10-million-password-list-top-1000000.txt";
+
+  public Producer(Password passwd)
+  {
+    this.passwd = passwd;
+  }
 
   @Override
   public void run()
   {
-    
+    passwd.getPassword();
   }
-  
+
+  public String Load() throws FileNotFoundException, IOException
+  {
+    String passwd = "";
+    
+    BufferedReader br = new BufferedReader(new FileReader("test.txt"));
+    
+    while((br.readLine()) != null)
+    {
+      passwd = br.readLine();
+    }
+    
+    br.close();
+
+    return passwd;
+  }
 }
